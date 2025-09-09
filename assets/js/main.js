cac9249816f9a4dea2fa644b89a3489cfb3b4666
JayeshNeosoftTech <96480547+JayeshNeosoftTech@users.noreply.github.com>
2025-09-09 19:51:58 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const dropdownButtons = document.querySelectorAll(".sidebar-subMenu-listItem");
+    const dropdownButtons = document.querySelectorAll(".sidebar-subMenu-listItem");
 
-  dropdownButtons.forEach((btn) => {
-    btn.addEventListener("click", function () {
-      const sublist = this.closest("li").querySelector(".sidebar-subMenu-Sublist");
+    dropdownButtons.forEach((btn) => {
+        btn.addEventListener("click", function () {
+        const sublist = this.closest("li").querySelector(".sidebar-subMenu-Sublist");
 
-      this.classList.toggle("active");
+        this.classList.toggle("active");
 
-      if (sublist.style.display === "block") {
-        sublist.style.display = "none";
-      } else {
-        sublist.style.display = "block";
-      }
+        if (sublist.style.display === "block") {
+            sublist.style.display = "none";
+        } else {
+            sublist.style.display = "block";
+        }
+        });
     });
-  });
 
+    document.querySelector(".searchbar-box").setAttribute("disabled", "disabled");
 
     document.getElementById("subMenu-btn").addEventListener("click", function () {
         document.querySelector(".sidebar-subMenu").classList.toggle("open");
 
         if (!document.querySelector(".sidebar-subMenu").classList.contains("open")) {
-        // Remove active class from all items
-        document.querySelectorAll(".sidebar-subMenu-listItem").forEach((item) => {
-            item.classList.remove("active");
-        });
+            document.querySelectorAll(".sidebar-subMenu-listItem").forEach((item) => {
+                item.classList.remove("active");
+            });
 
-        document.querySelectorAll(".sidebar-subMenu-Sublist").forEach((sublist) => {
-            sublist.style.display = "none";
-        });
+            document.querySelectorAll(".sidebar-subMenu-Sublist").forEach((sublist) => {
+                sublist.style.display = "none";
+            });
+
+            document.querySelector(".searchbar-box").setAttribute("disabled", "disabled");
+        }
+        else{
+            document.querySelector(".searchbar-box").removeAttribute("disabled");
         }
     });
 
-    
+
     document.querySelectorAll(".mainMenu-listItem").forEach(item => {
         item.addEventListener("click", function () {
             document.querySelectorAll(".mainMenu-listItem").forEach(el => {
@@ -48,10 +53,31 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelector(".sidebar-subMenu-title").textContent = title;
 
             if (title === "Dashboard") {
-            document.querySelector(".dashboard-submenu").classList.add("active");
-            } else if (title === "Tasks") {
-            document.querySelector(".tasks-submenu").classList.add("active");
+                document.querySelector(".dashboard-submenu").classList.add("active");
+            } 
+
+            else if (title === "Tasks") {
+                document.querySelector(".tasks-submenu").classList.add("active");
             }
+
+            else if (title === "Setting") {
+                document.querySelector(".setting-submenu").classList.add("active");
+            }
+
         });
+    });
+
+
+    const toggleSwitch = document.getElementById("themeToggle");
+
+    toggleSwitch.addEventListener("change", function () {
+        document.body.classList.toggle("dark", this.checked);
+
+        if(document.body.classList.contains("dark")){
+            document.querySelector(".logo").setAttribute("src", "./assets/img/recruitCRM_logo.svg");
+        }
+        else{
+            document.querySelector(".logo").setAttribute("src", "./assets/img/recruitCRM_logo_blue.svg");
+        }
     });
 });
